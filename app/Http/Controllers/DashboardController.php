5fd8a8de9ba7f9cb27b9dbef9033d6efb42a2fee
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-09-27 10:11:17 +0700

    Removed the set time zone API

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,17 +4,10 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class DashboardController extends Controller
+class DashboardController extends Controller implements HasMiddleware
 {
-    public function setTimeZone(Request $request) {
-        $userTimezone = $request->timezone;
-
-        $currentTime = Carbon::now($userTimezone)->format('H');
-
-        return response()->json([ 'current_time' => $currentTime ]);
-    }
-
     public function index(Request $request) {
         return view('users.dashboard');
     }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,4 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::post('/set-timezone', [DashboardController::class, 'setTimeZone']);
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
