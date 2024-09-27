32c615ff275658642e2174e80bd9623cf7abb6e6
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-09-27 18:50:12 +0700

    Added emojis to convey that feeling of that time

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -7,19 +7,24 @@
                 const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
                 const time = (new Date()).getHours();
-                let phrase;
+                let phrase, emoji;
 
                 if(time >= 21) {
                     phrase = 'Good Night, ';
+                    emoji = '🌙'; 
                 } else if(time >= 17) {
                     phrase = 'Good Evening, ';
+                    emoji = '🕯️';
                 } else if(time >= 12) {
                     phrase = 'Good Afternoon, ';
+                    emoji = '☀️';
                 } else if(time >= 4) {
                     phrase = 'Good Morning, ';
+                    emoji = '☕';
                 }
 
                 document.querySelector('#current-time').innerText = phrase;
+                document.querySelector('#current-emoji').innerText = emoji;
             } catch(err) {
                 console.log(err);
             }
@@ -28,5 +33,11 @@
 
     <span id="current-time" class="h1"></span>
     <span class="h1">{{ auth()->user()->username }}</span>
+    <span class="h1 ms-3" id="current-emoji"></span>
+    <div class="mb-5 mt-5"></div>
+    
+    @if(auth()->user()->date_of_relapse === null)
+        <p>You haven't set a relapse date yet</p>
+    @endif
 
 </x-layout>
