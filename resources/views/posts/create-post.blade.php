69bffb2d589085cf0ad92ebb686d19385677982f
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-09-30 17:21:58 +0700

    Finished the create post form

--- resources/views/posts/create-post.blade.php
+++ resources/views/posts/create-post.blade.php
@@ -1,5 +1,18 @@
 <x-layout>
 
-    <h1>Test</h1>
+    <div class="mt-7"></div>
+
+    <div class="container">
+        <h1 class="mb-5">Create a post</h1>
+
+        <form method="POST" action="{{ route('posts.store') }}">
+            @csrf
+            
+            <input type="text" class="form-control mb-1" name="title" placeholder="Title goes here...">
+            <textarea class="form-control mb-1" name="body" placeholder="And whatever you are thinking goes here!"></textarea>
+
+            <button class="btn btn-primary float-end">Create post</button>
+        </form>
+    </div>
 
 </x-layout>
