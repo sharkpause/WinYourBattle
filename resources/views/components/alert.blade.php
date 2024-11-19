795c9cba2b90eb7c3dd00774bcfcfa5f068e3e36
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-11-19 18:01:56 +0700

    Fixed the alert not sliding back up

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,7 @@
 @props(['msg' => 'Unknown alert', 'classes' => 'alert-primary'])
 
 <div class="container">
-    <div class="mx-auto alert fixed-top sharp-corners text-center fs-5 {{ $classes }}" role="alert">
+    <div class="mx-auto alert fixed-top sharp-corners text-center fs-5 alert-elem {{ $classes }}" role="alert">
         {{ $msg }}
     </div>
 </div>
