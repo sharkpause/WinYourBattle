a3b328b99090adfe1d9c432f266cdd25731a2efe
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-12-09 15:45:12 +0700

    Added a class prop to spinner for custom classes

--- resources/views/components/spinner.blade.php
+++ resources/views/components/spinner.blade.php
@@ -1,4 +1,6 @@
-<span class="spinner-elem">
+@props(['class' => ""])
+
+<span class="spinner-elem {{ $class }}">
   <div class="spinner-border text-muted spinner-border-sm" role="status">
     <span class="sr-only text-muted">Please wait...</span>
   </div>
