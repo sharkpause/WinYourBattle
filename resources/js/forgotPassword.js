985b7ef07a9e25391d2d85a253830d0cf34af78a
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-12-11 11:29:46 +0700

    Fixed the showSpinner function call to pass the parent element

--- resources/js/forgotPassword.js
+++ resources/js/forgotPassword.js
@@ -5,5 +5,5 @@ import './components/submitButtonUnclickable.js';
 import showSpinner from './components/spinner.js';
 
 $('.form-with-spinner').on('submit', () => {
-    showSpinner();
+    showSpinner(this);
 });
--- resources/js/login.js
+++ resources/js/login.js
@@ -5,5 +5,5 @@ import './components/submitButtonUnclickable.js';
 import showSpinner from './components/spinner.js';
 
 $('.form-with-spinner').on('submit', function(e) {
-    showSpinner();
+    showSpinner(this);
 });
