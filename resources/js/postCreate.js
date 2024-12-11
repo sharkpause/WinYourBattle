7383f2220f38d3cc5db8d4b8c774cbde0bba8038
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-12-11 11:21:54 +0700

    Removed an unnecessary document ready check function and removed the chatgpt comments

--- resources/js/postCreate.js
+++ resources/js/postCreate.js
@@ -2,35 +2,30 @@ import $ from 'jquery';
 
 import './components/autoResizeTextarea.js';
 
-$(document).ready(function () {
-    $('#post_image').on('change', function () {
-        const files = this.files;
+$('#post_image').on('change', function () {
+    const files = this.files;
 
-        if (files && files[0]) {
-            const fileReader = new FileReader();
+    if (files && files[0]) {
+        const fileReader = new FileReader();
 
-            fileReader.onload = function (e) {
-                // Clear previous preview
-                $('#image-preview-container').empty();
+        fileReader.onload = function (e) {
+            $('#image-preview-container').empty();
 
-                // Create a new image element
-                const img = $('<img>', {
-                    src: e.target.result,
-                    alt: 'Image Preview',
-                    css: {
-                        maxWidth: '500px',
-                        maxHeight: '500px',
-                        borderRadius: '10px',
-                        border: '1px solid #ddd',
-                        objectFit: 'cover',
-                    }
-                });
+            const img = $('<img>', {
+                src: e.target.result,
+                alt: 'Image Preview',
+                css: {
+                    maxWidth: '500px',
+                    maxHeight: '500px',
+                    borderRadius: '10px',
+                    border: '1px solid #ddd',
+                    objectFit: 'cover',
+                }
+            });
 
-                // Append the image to the container
-                $('#image-preview-container').append(img);
-            };
+            $('#image-preview-container').append(img);
+        };
 
-            fileReader.readAsDataURL(files[0]);
-        }
-    });
+        fileReader.readAsDataURL(files[0]);
+    }
 });
