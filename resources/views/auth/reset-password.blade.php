cf36bf267b05f72fa5e5a2f2d28042bf6ce45ce9
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-12-11 12:29:37 +0700

    Moved the vite import to the very bottom

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,41 +1,41 @@
 <x-layout>
-    <div class="container">
-      @if(session('status'))
-        <x-alert msg="{{ session('status') }}" classes="alert-elem success-alert text-white"></x-alert>
-      @endif
-      @if(session('email'))
-        <x-alert msg="{{ session('email') }}" classes="alert-danger alert-elem" ></x-alert>
-      @endif
+  <div class="container">
+    @if(session('status'))
+      <x-alert msg="{{ session('status') }}" classes="alert-elem success-alert text-white"></x-alert>
+    @endif
+    @if(session('email'))
+      <x-alert msg="{{ session('email') }}" classes="alert-danger alert-elem" ></x-alert>
+    @endif
     
-      <div class="d-flex flex-column min-vh-80 justify-content-center align-items-center"><div class="card col-4 border-radius-2-rem shadow">
-          <div class="card-body m-3">
-            <h3 class="mb-5">Reset password</h3>
-            <form method="POST" action="{{ route('password.update') }}" class="form-with-spinner">
-                @csrf
+    <div class="d-flex flex-column min-vh-80 justify-content-center align-items-center"><div class="card col-4 border-radius-2-rem shadow">
+        <div class="card-body m-3">
+          <h3 class="mb-5">Reset password</h3>
+          <form method="POST" action="{{ route('password.update') }}" class="form-with-spinner">
+              @csrf
 
-                <input type="hidden" name="token" value="{{ $token }}">
-                <input type="hidden" name="email" value="{{ request()->query('email') }}">
+              <input type="hidden" name="token" value="{{ $token }}">
+              <input type="hidden" name="email" value="{{ request()->query('email') }}">
         
-                <div class="form-group mb-4">
-                  <input type="password" name="password" class="form-control ps-0 underline-form @error('password') error-underline @enderror" placeholder="Password">
-                  @error('password')
-                      <p class="text-danger">{{ $message }}</p>
-                  @enderror
-                </div>
+              <div class="form-group mb-4">
+                <input type="password" name="password" class="form-control ps-0 underline-form @error('password') error-underline @enderror" placeholder="Password">
+                @error('password')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+              </div>
   
-                <div class="form-group mb-5">
-                  <input type="password" name="password_confirmation" class="form-control ps-0 underline-form @error('password') error-underline @enderror" placeholder="Confirm Password">
-                  @error('password') @enderror
-                </div>
+              <div class="form-group mb-5">
+                <input type="password" name="password_confirmation" class="form-control ps-0 underline-form @error('password') error-underline @enderror" placeholder="Confirm Password">
+                @error('password') @enderror
+              </div>
         
-                <button type="submit" class="width-100 btn btn-primary button-click-animation submit-button">
-                  <x-spinner></x-spinner>
-                  <span class="text-center w-100">Reset my password</span>
-                </button>
-              </form>
-          </div>
-    </div></div></div>
+              <button type="submit" class="width-100 btn btn-primary button-click-animation submit-button">
+                <x-spinner></x-spinner>
+                <span class="text-center w-100">Reset my password</span>
+              </button>
+            </form>
+        </div>
+  </div></div></div>
   
-    @vite('resources/js/resetPassword.js')
-  
-  </x-layout>
+</x-layout>
+
+@vite('resources/js/resetPassword.js')
