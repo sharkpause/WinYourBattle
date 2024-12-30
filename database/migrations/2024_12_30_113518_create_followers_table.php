1934dd1f8439d1dc9ee8bc22f0d39f0a1bf60a07
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2024-12-30 18:42:32 +0700

    Craeted the appropriate configurations in the migrations file of Follower

--- database/migrations/2024_12_30_113518_create_followers_table.php
+++ database/migrations/2024_12_30_113518_create_followers_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
+                
+            $table->foreignID('user_id')->on('users')->onDelete('cascade');
+            $table->foreignID('follower_id')->on('users')->onDelete('cascade');
+            $table->unique(['user_id', 'follower_id']);
+
             $table->timestamps();
         });
     }
