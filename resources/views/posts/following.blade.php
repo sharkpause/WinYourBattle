f1c2d9e78cfaf5b7d36cf29dc330f3ebae557b94
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-01-02 08:30:49 +0700

    Created the view to display user followed posts

---
+++ resources/views/posts/following.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+
+    <x-navbarSpace></x-navbarSpace>
+
+    <div class="container">
+        @if(session('success'))
+            <x-alert msg="{{ session('success') }}" classes="text-white success-alert"></x-alert>
+        @endif
+        @if(session('error'))
+            <x-alert msg="{{ session('success') }}" classes="text-white danger-alert"></x-alert>
+        @endif
+
+        <div class="mb-5">
+            <span class="h1 me-3">Latest Followed Posts</span>
+            <span class="text-muted">Catch up on your followings!</span>
+        </div>
+
+        @auth
+            <input type="text"
+                autocomplete="off"
+                class="shadow p-3 form-control border-radius-2-rem mb-5 button-click-animation-sm"
+                id="user-post-input"
+                placeholder="What are you thinking right now?"
+                data-url="{{ route('posts.create') }}">
+        @endauth
+
+        @foreach($posts as $post)
+            <x-postCard :post="$post"></x-postCard>
+        @endforeach
+    </div>
+
+    <x-paginator :items="$posts"></x-paginator>
+
+</x-layout>
+
+@vite('resources/js/index.js')
