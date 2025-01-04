c09cb67f08e308c568f51b54ed8cea8937a0e5e6
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-01-04 11:35:13 +0700

    Changed the use circle notch instead of spinner and changed the mood text

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -98,12 +98,12 @@
             <div class="col-md-2 my-3 me-1 border-radius-1-rem shadow d-flex flex-column align-items-center max-width-10-percent">
                 <p class="text-center mt-3 h3 text-muted">Mood</p>
                 <p class="text-center text-muted" id="mood-selected-date"></p>
-                <i class="fa-solid fa-spinner mt-5 font-size-100px spin"
+                <i class="fa-solid fa-circle-notch mt-5 font-size-100px spin"
                    id="mood-icon"
                    data-url="{{ route('set-mood') }}"
                    data-csrf-token="{{ csrf_token() }}"></i>
                 <p class="text-center mt-3 text-wrap" id="mood-text">
-                    Loading... Hold on!
+                    Loading...
                 </p>
             </div>
             
