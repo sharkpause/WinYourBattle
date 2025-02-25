3d955589f0fdf47f959bfef60b927000ec91c311
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-02-25 19:55:34 +0700

    feat: Created the appropriate field in follow requests migration

--- database/migrations/2025_01_24_100402_create_follow_requests_table.php
+++ database/migrations/2025_01_24_100402_create_follow_requests_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('follow_requests', function (Blueprint $table) {
             $table->id();
+            
+            $table->foreignID('user_id')->on('users');
+
             $table->timestamps();
         });
     }
