db60dc9d9f1198d20bafe752a8934c5544694838
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-03-03 12:49:27 +0700

    feat: add accept and reject button but without functionality

--- resources/js/layout.js
+++ resources/js/layout.js
@@ -3,6 +3,7 @@ import Swal from 'sweetalert2';
 import axios from 'axios';
 
 let followerListPage = 0;
+let followRequestCount = 0;
 
 $('#follow-requests-button').on('click', async function(e) {
     const customAlert = Swal.mixin({
@@ -34,16 +35,23 @@ $('#follow-requests-button').on('click', async function(e) {
             followRequestList.empty();
             for(let i = 0; i < followers.length; ++i) {
                 followRequestList.append(`
-                    <li class="d-flex align-items-center justify-content-between p-2">
+                    <li class="d-flex align-items-center justify-content-between p-2 account-item">
                         <div>
                             <img src="${followers[i].image_url}" class="rounded-circle me-2" width="30" height="30">
                             <strong>${followers[i].username}</strong>
                         </div>
-                        <button class="btn btn-primary follow-button follower-list-follow-button"
-                            <strong>Accept</strong>
-                        </button>
+                        <div>
+                            <button class="btn btn-primary accept-follow-request-button" data-accepted="false">
+                                <strong>Accept</strong>
+                            </button>
+                            <button class="btn btn-danger reject-follow-request-button">
+                                <strong>Reject</strong>
+                            </button>
+                        </div>
                     </li>
                 `);
+
+                ++followRequestCount;
 
                 // if(followers[i].followedByAuth) {
                 //     const followerListFollowButton = $('.follower-list-follow-button');
@@ -65,4 +73,22 @@ $('#follow-requests-button').on('click', async function(e) {
     //         commentCard.show();
     //     }
     // }
+});
+
+$(document).on('click', '.accept-follow-request-button', async function(e) {
+    if($(this).attr('data-accepted') === 'false') {
+        $(this).text('Follow Back')
+    } else {
+        alert('b')
+    }
+});
+
+$(document).on('click', '.reject-follow-request-button', async function(e) {
+    const accountItem = $(this).closest('.account-item');
+    accountItem.addClass('hidden');
+
+    --followRequestCount;
+    if(followRequestCount === 0) {
+        $('#follow-request-list').append('<p class="text-muted mt-3">No follow requests</p>');
+    }
 });
