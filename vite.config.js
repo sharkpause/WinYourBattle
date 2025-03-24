ba3220cb3a84cbe6627d4c40876687d681d1c247
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-03-24 21:39:13 +0700

    build: add production build assets

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import glob from 'fast-glob';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: glob.sync('resources/{js,css}/**/*.{js,css}'),
+        },
+    },
 });
