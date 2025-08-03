cee0e83d925e2b6f2f89e83f1f2e72cdc83b27cc
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-08-03 22:33:33 +0700

    feature: add terms and conditions text

--- resources/views/legal/terms.blade.php
+++ resources/views/legal/terms.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+<x-layout>
+
+<x-navbarSpace></x-navbarSpace>
+<div class="container card col-5 border-radius-2-rem shadow p-4">
+
+    <strong><p>Effective Date: 3rd August, 2025</p></strong>
+    <strong><p>Welcome to WinYourBattle. By using this app, you agree to the following terms:</p></strong>
+
+    <p><strong>1. Use of the App</strong></p>
+    <p>We collect the following data:<br>
+    <span class="ms-4">WinYourBattle is a free tool to help you track addiction recovery. You may use it for personal, non-commercial purposes. Don’t abuse the app, try to hack it, or use it to harm others.<br></span>
+    </p>
+
+    <p><strong>2. Accounts and Data</strong></p>
+
+    <p>Your data is used only to:<br>
+        <span class="ms-4">You may create an account using email, Google, or GitHub. You’re responsible for keeping your login secure. You may delete your account and associated data at any time.<br></span>
+    </p>
+
+    <p><strong>3. Content</strong></p>
+    <p>You are responsible for the journal entries, images, and content you upload. Don’t upload anything harmful, abusive, or illegal.</p>
+
+    <p><strong>4. Minors</strong></p>
+    <p>We allow users under 18, but recommend supervision for users under 13.<br>
+
+    <p><strong>7. Changes</strong></p>
+    <p>We may update these terms as the app grows. Continued use of the app means you agree to the latest version.</p>
+
 </div>
+
+</x-layout>
